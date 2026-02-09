9a51dc216a24016a59c438b092aef3230b41fa51
Tumaini21 <harkishramesh4859@gmail.com>
2026-02-09 09:45:26 +0000

    game

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
+
+        int a = 5;
+        int b = 7;
+        int sum = a + b;
+
+        System.out.println("Sum of a + b = " + sum);
     }
 }
